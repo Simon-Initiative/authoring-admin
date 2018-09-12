0ee5408efc3ddfa733131704df4d73223569c854
Raphael Gachuhi <rgachuhi@cmu.edu>
2018-09-12 11:37:12 -0400

    package lock/hide feature implementation

--- src/Data/PackageDetails.elm
+++ src/Data/PackageDetails.elm
@@ -1,18 +1,21 @@
-module Data.PackageDetails exposing (PackageDetails, retrievePackageDetails)
+module Data.PackageDetails exposing (PackageDetails, retrievePackageDetails, hidePackage, lockPackage, Locked, Hidden)
 
 import Data.Guid exposing (Guid, decoder)
 import Data.Resource exposing (Resource, resourcesDecoder)
 import Data.ResourceId exposing (ResourceId, decoder, toString)
 import Html exposing (..)
 import Http
-import Json.Decode exposing (Decoder, fail, float, int, list, nullable, string, succeed)
+import Json.Decode exposing (Decoder, fail, float, int, list, nullable, string, succeed, bool)
 import Json.Decode.Pipeline exposing (hardcoded, optional, required)
-
+import Json.Encode as Encode
+import Url.Builder as Url
 
 type alias PackageDetails =
     { guid : Guid
     , id : ResourceId
     , title : String
+    , visible: Bool
+    , editable: Bool
     , resources : List Resource
     }
 
@@ -53,4 +56,94 @@ detailsDecoder =
         |> required "guid" Data.Guid.decoder
         |> required "id" Data.ResourceId.decoder
         |> required "title" string
+        |> required "visible" bool
+        |> required "editable" bool
         |> required "resources" resourcesDecoder
+
+hidePackage: Guid -> Bool -> String -> Http.Request Hidden
+hidePackage courseId hide token =
+    let
+        headers =
+            [ Http.header
+                "Content-Type"
+                "application/json"
+            , Http.header
+                "Accept"
+                "application/json"
+            , Http.header
+                "Authorization"
+                ("Bearer "
+                    ++ token
+                )
+            ]
+
+
+        url = Url.crossOrigin "http://dev.local" ["api", "v1", "packages", Data.Guid.toString courseId, "hide"]
+            [Url.string "hide" ((\n -> if n then "true" else "false") hide)]
+
+        body = Encode.list Encode.string [Data.Guid.toString courseId]
+                |> Http.jsonBody
+    in
+    Http.request
+        { method = "GET"
+        , headers = headers
+        , url = url
+        , body = body
+        , expect = Http.expectJson hiddenDecoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+lockPackage: Guid -> Bool -> String -> Http.Request Locked
+lockPackage courseId lock token =
+    let
+        headers =
+            [ Http.header
+                "Content-Type"
+                "application/json"
+            , Http.header
+                "Accept"
+                "application/json"
+            , Http.header
+                "Authorization"
+                ("Bearer "
+                    ++ token
+                )
+            ]
+
+        url = Url.crossOrigin "http://dev.local" ["api", "v1", "packages", Data.Guid.toString courseId, "lock"]
+            [Url.string "lock" ((\n -> if n then "true" else "false") lock)]
+
+        body = Encode.list Encode.string [Data.Guid.toString courseId]
+                |> Http.jsonBody
+    in
+    Http.request
+        { method = "POST"
+        , headers = headers
+        , url = url
+        , body = body
+        , expect = Http.expectJson lockDecoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+type alias Locked =
+    { locked : String
+    , packages : List String
+    }
+
+lockDecoder : Decoder Locked
+lockDecoder =
+  succeed Locked
+          |> required "locked" string
+          |> required "packages" (list string)
+
+type alias Hidden =
+    { hidden : String
+    , packages : List String
+    }
+hiddenDecoder : Decoder Hidden
+hiddenDecoder =
+  succeed Hidden
+          |> required "hidden" string
+          |> required "packages" (list string)
--- src/Page/PackageDetails.elm
+++ src/Page/PackageDetails.elm
@@ -3,13 +3,13 @@ module Page.PackageDetails exposing (Model, Msg, init, subscriptions, toSession,
 import Browser.Navigation as Nav
 import Data.Guid as Guid exposing (Guid)
 import Data.Package as Package exposing (Package)
-import Data.PackageDetails as PackageDetails exposing (PackageDetails, retrievePackageDetails)
+import Data.PackageDetails as PackageDetails exposing (PackageDetails, retrievePackageDetails, hidePackage, lockPackage)
 import Data.Resource as Resource exposing (Resource, ResourceState)
 import Data.ResourceId as ResourceId exposing (ResourceId)
 import Data.Username as Username exposing (Username)
-import Html exposing (Html, button, div, fieldset, h1, h3, input, li, text, textarea, ul)
-import Html.Attributes exposing (attribute, class, placeholder, type_, value)
-import Html.Events exposing (onInput, onSubmit)
+import Html exposing (Html, button, div, fieldset, h1, h3, input, li, text, textarea, ul, label)
+import Html.Attributes exposing (attribute, class, placeholder, type_, value, checked)
+import Html.Events exposing (onInput, onSubmit, onClick)
 import Http
 import Json.Decode as Decode exposing (Decoder, decodeString, field, list, string)
 import Json.Decode.Pipeline exposing (hardcoded, required)
@@ -19,8 +19,6 @@ import Log
 import Route
 import Session exposing (Session)
 import Task
-
-
 
 -- MODEL
 
@@ -38,6 +36,7 @@ type Status
     | Failed Http.Error
 
 
+
 init : Guid -> Session -> ( Model, Cmd Msg )
 init packageId session =
     ( { session = session
@@ -50,6 +49,13 @@ init packageId session =
         ]
     )
 
+type Msg
+    = RetrievedDetails (Result Http.Error PackageDetails)
+    | PassedSlowLoadThreshold
+    | ToggleVisible PackageDetails
+    | ToggleEditable PackageDetails
+    | LockPermission (Result Http.Error PackageDetails.Locked)
+    | HidePermission (Result Http.Error PackageDetails.Hidden)
 
 
 -- VIEW
@@ -59,7 +65,18 @@ viewDetails : PackageDetails -> Html Msg
 viewDetails details =
     div []
         [ h3 [] [ text details.title ]
-        , viewResources details.resources
+        , div [] [
+            label [] [
+                input [type_ "checkbox"
+                , checked <| details.visible
+                , onClick <| ToggleVisible details][], text <| " visible"
+            ]
+            , label [] [
+                input [type_ "checkbox"
+                , checked <| details.editable
+                , onClick <| ToggleEditable details][], text <| " editable"]
+            ],
+            viewResources details.resources
         ]
 
 
@@ -101,11 +118,6 @@ view model =
 -- UPDATE
 
 
-type Msg
-    = RetrievedDetails (Result Http.Error PackageDetails)
-    | PassedSlowLoadThreshold
-
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -119,6 +131,26 @@ update msg model =
             , Cmd.none
             )
 
+        ToggleVisible details ->
+            let
+                viz = toggle details.visible
+            in
+            ({model | status =  Loaded {details | visible = viz}}
+            , Cmd.batch
+                [ hidePackage details.guid viz model.session.token
+                    |> Http.send HidePermission
+                ])
+
+        ToggleEditable details ->
+            let
+                loc = toggle details.editable
+            in
+             ({model | status = Loaded {details | editable = loc}}
+            , Cmd.batch
+                [ lockPackage details.guid loc model.session.token
+                    |> Http.send LockPermission
+                ])
+
         PassedSlowLoadThreshold ->
             case model.status of
                 Loading ->
@@ -130,6 +162,9 @@ update msg model =
                     ( model, Cmd.none )
 
 
+toggle: Bool -> Bool
+toggle bool =
+    not bool
 
 -- SUBSCRIPTIONS
 
